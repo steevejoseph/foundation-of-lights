638315bed44b25217ffa458a238041713ab0c33e
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-11-28 14:22:48 -0500

    Copy over construction plans page

---
+++ src/pages/construction-plans/index.tsx
@@ -0,0 +1,23 @@
+import Layout from "src/components/Layout";
+
+const ConstructionPlans = () => {
+  return (
+    <Layout>
+      <h2>Expansion of Masjid al-Tazkiah </h2>
+      <p>
+        Masjid al-Tazkiah has been proudly serving this community for 24 years.
+        With the growing community, we have long hoped to expand the Masjid to
+        prevent overcrowding. In addition, the expansion would allow us to add
+        more space for individuals to offer their prayers. Alhamdulillah, with
+        the help of Allah, your donations have allowed us to start the expansion
+        project. We kindly request the community to help us to continue and
+        complete this project. By donating right now, you will receive the
+        reward of a house in paradise. We appreciate the generous donation and
+        we look forward to continue and completing this project soon. May
+        Allah(SWT)reward you enormously.{" "}
+      </p>
+    </Layout>
+  );
+};
+
+export default ConstructionPlans;
