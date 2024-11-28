8aea015441ee0728052a8d326768a89c0d421d12
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-11-28 14:22:22 -0500

    Copy over imam page

--- src/pages/about/imam/index.tsx
+++ src/pages/about/imam/index.tsx
@@ -1,3 +1,66 @@
-export default function Imam() {
-  return <>imam page</>;
-}
+import Layout from "src/components/Layout";
+
+const Imam = () => {
+  return (
+    <Layout>
+      <div
+        className="about-history"
+        style={{
+          margin: "2em",
+        }}
+      >
+        <h2>Imam Ibraim Habach</h2>
+
+        <strong>Objective:</strong>
+        <p>
+          Develop and enhance Islamic education and Quran meaning and
+          memorization. Head the education committee.
+        </p>
+
+        <strong>Education:</strong>
+        <ul>
+          <li>
+            Hafez Quran with special authority to provide warrant endorsed by
+            renowned Sheikhs going back by lineage to Sahaba-Al-Keram.
+          </li>
+          <li>Bachelor Degree of Islamic legislation and Arabic Language</li>
+          <li>Special Diploma of Quran and Hadith (references and research)</li>
+        </ul>
+
+        <strong>Experience:</strong>
+        <ul>
+          <li>
+            1983-1990 Imam and Khateeb in Masjed Lala Basha (1500 Attendance)
+          </li>
+          <li>
+            1985- 1990 Director of Quran Memorization Institute (20 new Hafez a
+            year), Damascus Syria. <br />
+          </li>
+          <li>
+            1991-1993 Was invited by ISB to lead traweeh Prayer in Ramadan.{" "}
+            <br />
+          </li>
+          <li>
+            1991-1994 Teacher and assistance Imam in Al-Rahma school, Al-Haq
+            Masjed and Beriltz language center in Baltimore <br />
+          </li>
+          <li>
+            1994-2003: Imam, teacher and council member of Islamic Society of
+            Central Florida. (Orlando). <br />
+          </li>
+          <li>
+            Established full time school and three new Masjids under the
+            Umbrella of one organization, (Islamic Society of central Florida).{" "}
+            <br />
+          </li>
+          <li>
+            2003-present: founder, Imam, director, board member of Foundation of
+            Lights Masjidu Tazkia (Non profit organization) Ocoee, FL 34761
+          </li>
+        </ul>
+      </div>
+    </Layout>
+  );
+};
+
+export default Imam;
