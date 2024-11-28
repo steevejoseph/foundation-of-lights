f070a0be306601af6e2073876e4e61670d477d24
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-11-28 14:22:35 -0500

    Copy over about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,99 @@
+import Layout from "src/components/Layout";
+
 export default function About() {
-  return <>about page</>;
+  return (
+    <Layout>
+      <div
+        className="about-history"
+        style={{
+          margin: "2em",
+        }}
+      >
+        <h2>History of Masjid Tazkiah</h2>
+        <p>
+          Masjid Tazkiah, a beautiful masjid nestled in the heart of Ocoee,
+          Florida, has been a spiritual haven for the past 19 years,
+          Alhamdulillah. The journey to establish this serene sanctuary was not
+          without its challenges. It all began with a blessed idea that led to
+          the incorporation of Foundation of Lights Inc. as a nonprofit
+          organization in the State of Florida on November 17, 2003. About a
+          year later, on September 22, 2004, the organization acquired the
+          historic United Oak Methodist Church building, located at 120 Floral
+          Street in Ocoee, Florida, and meticulously transformed it into a
+          masjid that radiates peace and tranquility. As word of the new
+          establishment spread, many members of the community, near and far,
+          were drawn to Masjid Al Tazkiah, and by the grace of Allah, the
+          congregation grew steadily. To ensure the masjid’s continued success
+          and smooth operations, a constant flow of funds was necessary. On
+          November 17, 2005, the Internal Revenue Service graciously granted the
+          organization a 501(c)(3) tax-exempt status, making all donations tax
+          deductible and encouraging increased support. In a noble effort to
+          create a verdant, inviting oasis for worshippers, the organization
+          acquired the property and land at 102 Floral Street on November 10,
+          2008. With the influx of devoted community members, additional space
+          was needed to accommodate the growing congregation. Acknowledging
+          this, the organization purchased 137 S Cumberland Avenue on November
+          21, 2011. Today, the Masjid is not only a place of worship but also a
+          beacon of hope and support for those in need. A Food Pantry operates
+          on the premises, generously providing sustenance to low-income and
+          needy families in the community regardless of their religions, race
+          and ethnicity. The story of Masjid Al Tazkiah is a testament to the
+          power of faith, unity, and perseverance in the face of challenges, all
+          under the watchful eyes of Allah.
+        </p>
+      </div>
+
+      <div
+        className="about-mission"
+        style={{
+          margin: "2em",
+        }}
+      >
+        <h2>Our Mission at Masjid Tazkiah</h2>
+        <p>
+          Our mission is to promote the teaching of Islam as a source of
+          guidance, wisdom, and compassion for individuals and communities. We
+          strive to educate, empower, and uplift people through the principles
+          of Islam, morality, and social responsibilities. Our objective is to
+          seek the glory of Allah (SWT), our Prophet Muhammad (Peace be upon
+          him), and the Quran and Sunnah.
+        </p>
+      </div>
+
+      <div
+        className="about-vision"
+        style={{
+          margin: "2em",
+        }}
+      >
+        <h2>Our Vision</h2>
+        <p>
+          Our vision is to convey a clear perception and understanding of
+          Islamic teachings to both Muslims and non-Muslims. Our spiritual
+          vision is to pursue faith, knowledge, and righteousness in a peaceful
+          and caring environment. <br /> <br />
+          We strive to be:
+          <ol>
+            <li>
+              1. A secure and comfortable facility where Muslims gather for the
+              worship of Allah, regardless of race, nationality, ethnicity,
+              gender, or age. <br />
+            </li>
+            <li>
+              2. A facility that serves and interacts with Muslim and non-Muslim
+              communities in accordance with the teachings of Islam. <br />
+            </li>
+            <li>
+              3. An organization where charitable causes are supported. <br />
+            </li>
+            <li>
+              4. An interfaith place that provides a venue for members of
+              different faiths to interact in a peaceful and harmonious
+              atmosphere
+            </li>
+          </ol>
+        </p>
+      </div>
+    </Layout>
+  );
 }
