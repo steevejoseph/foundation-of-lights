46b0e65b64ca9650ea42cdbf1431ec3ddb348066
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-11-28 22:56:12 -0500

    Refactor styles in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,88 +4,30 @@ import styles from "../styles/footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div
-        className="info-container"
-        style={{
-          display: "flex",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: "100%",
-        }}
-      >
-        <div
-          className="info-address"
-          style={{
-            width: "50%",
-            flexDirection: "column",
-            justifyContent: "space-evenly",
-            fontSize: "14px",
-          }}
-        >
-          <h3
-            style={{
-              fontSize: "24px",
-              fontWeight: "600",
-              marginBottom: "20px",
-              color: "#fff",
-            }}
-          >
-            Foundation of Lights
-          </h3>
+      <div className={styles.infoContainer}>
+        <div className={styles.infoAddress}>
+          <h3 className={styles.infoTitle}>Foundation of Lights</h3>
 
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "8px", // Adds consistent spacing between address lines
-              color: "#fff",
-            }}
-          >
+          <div className={styles.addressContainer}>
             <p>Masjid Tazkiah</p>
             <p>120 Floral St,</p>
             <p>Ocoee, FL 34761</p>
           </div>
         </div>
 
-        <div
-          className="info-about-donate"
-          style={{
-            width: "50%",
-            flexDirection: "column",
-            gap: "8px",
-          }}
-        >
-          <h4
-            style={{
-              fontSize: "20px",
-              fontWeight: "normal",
-              marginBottom: "16px",
-              color: "#fff",
-            }}
-          >
-            About
-          </h4>
+        <div className={styles.infoAboutDonate}>
+          <h4 className={styles.aboutTitle}>About</h4>
 
           <Link
             href={"https://www.paypal.com/paypalme/foundationoflights"}
-            style={{
-              fontSize: "20px",
-              fontWeight: "normal",
-              color: "#fff",
-              textDecoration: "none",
-              display: "block",
-            }}
+            className={styles.donateLink}
           >
             Donate
           </Link>
         </div>
       </div>
 
-      <div
-        className="map-container"
-        style={{ width: "100%", height: "300px", margin: "20px 0" }}
-      >
+      <div className={styles.mapContainer}>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.2!2d-81.5444!3d28.5707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77e8f8e8e8e8f%3A0x0!2s120+Floral+St%2C+Ocoee%2C+FL+34761!5e0!3m2!1sen!2sus!4v1"
           width="100%"
@@ -97,7 +39,7 @@ const Footer = () => {
         ></iframe>
       </div>
 
-      <p style={{ textAlign: "center" }}>
+      <p className={styles.copyright}>
         &copy; 2024 Masjid Tazkiah. All Rights Reserved.
       </p>
     </footer>
