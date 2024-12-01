de2ddeed28c9b0d8d119f93bf6ce1919ed161349
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-12-01 11:33:56 -0500

    Fix dollar sign alignment

--- src/components/forms/donation/donation-form.tsx
+++ src/components/forms/donation/donation-form.tsx
@@ -92,7 +92,7 @@ export default function DonationForm() {
               Amount (USD)
             </label>
             <div className="relative flex-1">
-              <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500">
+              <span className="absolute left-3 top-[50%] -translate-y-[40%] text-gray-500">
                 $
               </span>
               <input
