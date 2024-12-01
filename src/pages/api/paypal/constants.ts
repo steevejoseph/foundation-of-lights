5392c0e9df3e61ba487df43cb523e129a36defb8
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-12-01 16:25:09 -0500

    Add forgotten file

--- src/pages/api/paypal/constants.ts
+++ src/pages/api/paypal/constants.ts
@@ -1,6 +1,19 @@
-export const PAYPAL_BASE_URL =
-  process.env.NODE_ENV === "production"
+import { env } from "~/env";
+
+export const getPaypalBaseUrl = () => {
+  return env.NODE_ENV === "production"
     ? "https://api-m.paypal.com"
     : "https://api-m.sandbox.paypal.com";
+};
 
-// export const PAYPAL_BASE_URL = "https://api-m.sandbox.paypal.com";
+// export const getPaypalClientId = () => {
+//   return env.NODE_ENV === "production"
+//     ? env.NEXT_PUBLIC_PAYPAL_PROD_CLIENT_ID
+//     : env.NEXT_PUBLIC_PAYPAL_DEV_CLIENT_ID;
+// };
+
+// export const getPaypalClientSecret = () => {
+//   return env.NODE_ENV === "production"
+//     ? env.NEXT_PAYPAL_PROD_CLIENT_SECRET
+//     : env.NEXT_PAYPAL_DEV_CLIENT_SECRET;
+// };
